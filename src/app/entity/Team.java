c49f6224ef55eea7d0cef93de048a420ba80f879
Henry Gonzales <henry.b.gonzales@obf.ateneo.edu>
2018-11-15 20:02:15 +0800

    added many-to-many for users and teams

---
+++ src/app/entity/Team.java
@@ -0,0 +1,25 @@
+package app.entity;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToMany;
+
+@Entity
+public class Team {
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private Long id;
+	
+	@Column
+	private String name;
+	
+	 @ManyToMany(mappedBy = "teams")
+	    private List<User> users = new ArrayList<>();
+	 
+}
--- src/app/entity/User.java
+++ src/app/entity/User.java
@@ -1,5 +1,40 @@
 package app.entity;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToMany;
+import javax.swing.text.html.HTML.Tag;
+import javax.persistence.JoinColumn;
+
+@Entity
 public class User {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private Long id;
+	
+	@Column
+	private String firstName;
+	
+	@Column
+	private String lastName;
+	
+	@ManyToMany(cascade = { 
+	        CascadeType.PERSIST, 
+	        CascadeType.MERGE
+	    })
+	    @JoinTable(name = "user_team",
+	        joinColumns = @JoinColumn(name = "user_id"),
+	        inverseJoinColumns = @JoinColumn(name = "team_id")
+	    )
+    private List<Team> teams = new ArrayList<>();
 
 }
