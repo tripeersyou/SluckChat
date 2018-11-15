02f31a3ee9253b05540f84dc3eb2dcedf7189b49
Francis Avancena <afrancisoliver@yahoo.com>
2018-11-15 14:52:26 +0800

    Secondary commit

---
+++ src/app/component/UserComponent.java
@@ -0,0 +1,5 @@
+package app.component;
+
+public class UserComponent {
+
+}
---
+++ src/app/rest/UsersController.java
@@ -0,0 +1,11 @@
+package app.rest;
+
+import javax.ws.rs.Path;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+@Path("/users")
+public class UsersController {
+
+}
