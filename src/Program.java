375b2eda3959d8c0deffd9f4997c08719996c2d1
Francis Avancena <afrancisoliver@yahoo.com>
2018-11-15 14:35:54 +0800

    Initial commit

---
+++ src/Program.java
@@ -0,0 +1,12 @@
+public class Program 
+{
+	public static void main(String[] args) throws Exception
+	{
+		int portNumber = 8000;
+		String restClassPackage = "app.rest";
+		String applicationContext = "applicationContext-jpa.xml";
+		System.out.println("Listening on http://localhost:8000");
+		new JerseyStarter().start(portNumber, restClassPackage, applicationContext);
+	}
+}
+
